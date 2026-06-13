e715497cb6cbcc195cb01cdff4c6ad1b428df1cb
estrada2 <115900228+estrada2@users.noreply.github.com>
2026-06-13 15:24:24 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service worker PWA.
 // Cachea el shell local para abrir la app offline y actualiza recursos cuando hay nueva version.
-const CACHE_NAME = 'vethome-pro-v10-schnauzer-2026-06-13-27';
+const CACHE_NAME = 'vethome-pro-v10-schnauzer-2026-06-13-28';
 const APP_SHELL = [
     '/',
     '/index.html',
